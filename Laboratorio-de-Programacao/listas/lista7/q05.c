8894ccc7667a5556e5b1b9db4d86b6c0821b664b
myller-silva <myllersilva1310@gmail.com>
2021-08-06 18:36:34 -0300

    Create q05.c

---
+++ Laboratorio-de-Programacao/listas/lista7/q05.c
@@ -0,0 +1,18 @@
+// Crie uma Estrutura disciplina em que um dos campos corresponde tambem a uma estrutura pro-
+// fessor.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct disciplina
+{
+    char nome[20];
+    struct professor
+    {
+        char nome[20];
+    };
+};
+int main(){
+        
+    return 0;
+}
