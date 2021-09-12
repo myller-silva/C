f48002a72753c49f799abc0cc99c2b18f8904f0b
myller-silva <myllersilva1310@gmail.com>
2021-09-12 10:17:45 -0300

    update

--- Laboratorio-de-Programacao/trabalho_arquivos/main.c
+++ Laboratorio-de-Programacao/trabalho_arquivos/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <string.h>
 
 typedef struct func{
 	int id;
@@ -9,17 +9,19 @@ typedef struct func{
 	float custo;
 }FUNC;
 
-void menu();
 int tot_pecas(char *);
 void atualizar_tot(char *, int );
+
+void menu();
 void inserir(char *, int );
 void listar(char *, int );
+void remover(char *, int );
 void atualizar(char *, int );
-void remover(char *, char *);
+
 
 int main(){
 	system("clear");
-	char *arq_tot_pecas = "tot_prod.txt";	
+	char *arq_tot_pecas = "tot_cad.txt";	
 	char *nome_arq = "hardware.dat";
 	int tot = tot_pecas( arq_tot_pecas );
 	int op;
@@ -31,28 +33,28 @@ int main(){
 			case 1:
 				inserir(nome_arq, tot);
 				tot++;
+				atualizar_tot(arq_tot_pecas, tot);
 				break;
 			case 2:
 				listar(nome_arq, tot);
 				break;
 			case 3:
-				puts("Ainda nao implementado");
-				// if(tot) remover(nome_arq, arq_tot_pecas);
-				// tot--;
+				if(tot) remover(nome_arq, tot);
+				tot--;
+				atualizar_tot(arq_tot_pecas, tot);
 				break;
 			case 4:
 				atualizar(nome_arq, tot);
 				break;
 		}
-		if(op>4){
+		if(op>4 || 0>op){
 			puts("Operacao invalida");
 		}
+		atualizar_tot(arq_tot_pecas, tot);
 	}
 	while(op);
-	atualizar_tot("tot_prod.txt", tot);
     return 0;
 }
-
 void menu(){
     puts("[1] Inserir");
     puts("[2] Listagem");
@@ -146,50 +148,57 @@ void atualizar(char *nome_arq, int tot){
     scanf("%d", &v[id].qt);
     printf("Custo atual: ");
     scanf("%f", &v[id].custo);
-
     // salvar:
     fp = fopen(nome_arq, "wb");
     if(!fp) perror("Erro");
-
     for(int c=0; c<tot; c++){
         fwrite(&v[c], sizeof(FUNC), 1, fp);        
-    }
-	
+    }	
     fclose(fp);
 	puts("\n>>>Ferramenta atualizada<<<\n");    
 }
-
-
-
-void remover(char *nome_arq, char *arq_tot_pecas){
-	FILE *fp = fopen(nome_arq, "rb");	
-	int tot = tot_pecas(arq_tot_pecas);
+void remover(char *nome_arq, int tot){
+	FILE *a = fopen(nome_arq, "rb");
+	
 	int e;
-
-	if(!fp){
-		perror("Erro");
-	}
-	FUNC o[tot];
-	FUNC n[tot-1];
-
+	if(!a){
+		perror("Erro");	
+		exit(8);
+	}	
+	// ler:
 	R:
-	printf("Digite o ID da ferramenta que deseja remover: ");
+	printf("ID da ferramenta que deseja remover: ");
 	scanf("%d", &e);
 	if(e<0 || tot<=e){
 		printf("ID invalido. Apenas numeros no intervalo de [0, %d]\n", tot-1);
-		puts("Tente novamente 3 segundos.");
-		sleep(3);
 		goto R;
 	}
-	fread(&o, sizeof(FUNC), 1, fp);
-	rewind(fp);
+	FUNC old[tot];
+	FUNC novo[tot-1];
+	// ler:	
+	for(int c=0; c<tot; c++){
+		fread(&old[c], sizeof(FUNC), 1, a);
+	}
+	fclose(a);
 	int k=0;
 	for(int c=0; c<tot; c++){
-		if(o[c].id == e){
+		if(c==e){
 			continue;
 		}
-		n[k] = o[c];
+		if(c<e){
+			novo[k].id = old[c].id;
+		}else{
+			novo[k].id = old[c].id - 1;
+		}		
+		strcpy(novo[k].nome, old[c].nome);
+		novo[k].qt = old[c].qt;
+		novo[k].custo = old[c].custo;
 		k++;
 	}
-	fclose(fp);
+	// escrever:	
+	FILE *b = fopen(nome_arq, "wb");	
+	for(int c=0; c<tot-1; c++){
+		fwrite(&novo[c], sizeof(FUNC), 1, b);
+	}
+	fclose(b);
 }
