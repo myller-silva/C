e167c5df78a4a38acef35f4d7f833052307372ec
myller-silva <myllersilva1310@gmail.com>
2021-11-01 17:35:24 -0300

    update

--- Outros/bibliotecas/vetores/main.c
+++ Outros/bibliotecas/vetores/main.c
@@ -1,59 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "vec_f.h"
-#include "vec_i.h"
 
-#define LS 99.99998
-#define LI 99.99997
+int *sort_i(int *v, int tam);
+void show_vec_i(int *v, int tam, char *formatacao);
 
 int main(int argc, char *argv[]){
-	if(argc!=3){
-		printf("Formato:\n\t%s <lin> <col>\n", argv[0]);
-		exit(1);
-	}
 
-	int lin = atoi(argv[1]);
-	int col = atoi(argv[2]);
-	// float *v = malloc( sizeof(float)*lin*col );
-	float v[] = {9.9, 7.5, 8.3, 9.9, 1.3, 9.1, 4.2, 0.3, 9.2};
-	float *ord = NULL;
+	int B[] = {71, 84, 52, 81, 89, 72, 80, 67, 120, 90, 80, 78, 69, 91, 71, 75, 75, 88, 78, 73,
+86, 78, 75, 73, 80, 78, 73, 81, 77, 82};
+	int *ord = NULL;
 
-	// v = random_vec_f(lin*col, LS, LI);
-	puts("\n");
-	show_vec_f(v, lin*col, "%f ");
-	puts("");
 	// ordenado:
-	ord = sort_f(v, lin*col);
-	show_vec_f(v, lin*col, "%f ");
-	puts("");
+	ord = sort_i(B, 30);
 
-	printf("\nmedia: %f\n", media_vec_f(v, lin*col));
-	printf("mediana: %f\n", mediana_vec_f(v, lin*col));
+	show_vec_i(ord, 30, "%d ");
 
-	printf("moda: ");
-	float *moda = malloc(sizeof(float)*lin*col);
-	for(int c=0; c<lin*col; c++){
-		moda[c] = v[c];
-	}
-	show_vec_f(moda, lin*col, "%f ");
-	puts("");
-
-	printf("maior: %f\n", max_vec_f(v, lin*col));
-	printf("menor: %f\n", min_vec_f(v, lin*col));
-	float n;
-	printf("Digite um numero para verificar se esta no vetor: ");
-	scanf("%f", &n);
-	
-	if(x_in_vec_f(v, lin*col, n)){
-		printf("%f esta em V\n", n);
-	}else{
-		printf("%f nao esta em V\n", n);
-	}
-
-	
-
-	
 
 
 	return 0;
+}
+
+
+void show_vec_i(int *v, int tam, char *formatacao){
+	for(int c=0; c<tam; c++){
+		printf(formatacao, *(v+c));
+	}
+}
+
+int *sort_i(int *v, int tam){
+
+	int *temp = malloc( sizeof(int)*tam );
+	int c, j, aux;
+
+	for(int c=0; c<tam; c++){
+		temp[c] = v[c];
+	}  
+	for( c=0; c<tam-1; c++ ){
+    for( j=c+1; j<tam; j++ ){
+      if( *(temp+j) < *(temp+c) ){
+        aux = *(temp+j) ;
+        *(temp+j) = *(temp+c) ;
+        *(temp+c) = aux ;
+      }
+    }
+  }
+
+	return temp;
 }
